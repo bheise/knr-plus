486c4baef43a5d2cc8275f364d4f345f2d148725
Ben Heise <ben@ip-10-122-74-52.ec2.internal>
2013-06-04 03:39:10 +0000

    lesson 1-6 added

---
+++ round2/1/1-6/hw.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* print characters */
+main () {
+
+  int c;
+  
+  c = getchar();
+  while (c != EOF) {
+    putchar(c);
+    printf("\n\n");
+    c = getchar();
+    printf("\n\n");
+
+  }
+
+}
+
+/* #include <stdio.h>
+
+main() {
+
+} */
